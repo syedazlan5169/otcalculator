94afd7abd314c8308a272d28e30ce315b7672ef8
logicgame1001 <logicgame1001@gmail.com>
2026-01-01 04:57:13 +0800

    fix: added ot-calculator2.js to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/js/ot-calculator.js",
+                "resources/js/ot-calculator2.js",
             ],
             refresh: true,
         }),
